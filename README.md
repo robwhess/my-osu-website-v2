5c6a7db9c0a34a74fbd262e739523b441649004c
Rob Hess <hessro@oregonstate.edu>
2024-03-07 13:38:38 -0800

    Initial commit after bootstrapping with create-vite, paring down to the essentials, and adding .htaccess.

---
+++ README.md
@@ -0,0 +1,3 @@
+# My OSU Website v2
+
+This is the next-generation version of my OSU website.
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <>
+      <h1>Rob Hess - Oregon State University</h1>
+    </>
+  )
+}
