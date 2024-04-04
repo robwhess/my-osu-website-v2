2f995a470cb745816bdc08076ccedf863fcc6bb7
Rob Hess <hessro@oregonstate.edu>
2024-04-04 12:54:14 -0700

    Set global body colors.

--- src/theme/index.js
+++ src/theme/index.js
@@ -2,6 +2,16 @@ import { extendTheme } from '@chakra-ui/react'
 
 import colors from './colors'
 
+const styles = {
+    global: {
+        body: {
+            bg: "gray.50",
+            color: "gray.900"
+        }
+    }
+}
+
 export default extendTheme({
+    styles,
     colors
 })
