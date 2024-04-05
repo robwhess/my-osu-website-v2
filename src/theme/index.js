b26a9bfa6a7f23b2dee9f1a658f8db421f163d0b
Rob Hess <hessro@oregonstate.edu>
2024-04-05 15:49:21 -0700

    Indent by 2 spaces.

--- src/theme/index.js
+++ src/theme/index.js
@@ -3,15 +3,15 @@ import { extendTheme } from '@chakra-ui/react'
 import colors from './colors'
 
 const styles = {
-    global: {
-        body: {
-            bg: "gray.50",
-            color: "gray.900"
-        }
+  global: {
+    body: {
+      bg: "gray.50",
+      color: "gray.900"
     }
+  }
 }
 
 export default extendTheme({
-    styles,
-    colors
+  styles,
+  colors
 })
