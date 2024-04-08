73cc2b0fd9c1927ff158c98a63dd3d842629db1d
Rob Hess <hessro@oregonstate.edu>
2024-04-08 15:25:31 -0700

    Small changes to header styling.  Decrease surrounding negative space in logo image files.

--- src/pages/Root/components/Header.jsx
+++ src/pages/Root/components/Header.jsx
@@ -8,12 +8,12 @@ import logo from '@/static/rh-logo-transparent.png'
 
 export default function Header() {
   return (
-    <HStack as="header" justify="end" align="start" spacing={1}>
+    <HStack as="header" justify="end" align="start" spacing={1} marginBlock={4} marginInline={2}>
       <VStack align="end" spacing={0}>
         <Heading as="h1" size="2xl" fontWeight="thin">Rob Hess</Heading>
-        <Heading as="h2" size={["md", "lg"]} fontWeight="thin">Oregon State University</Heading>
+        <Heading as="h2" size={["sm", "md"]} fontWeight="thin">Oregon State University</Heading>
       </VStack>
-      <Image boxSize={["75px", "96px"]} src={logo} alt="RH Logo" />
+      <Image boxSize={["70px", "80px"]} src={logo} alt="RH Logo" />
     </HStack>
   )
 }
