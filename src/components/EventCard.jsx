ec2c1dc438f628fc975f876efd929ea996159a62
Rob Hess <hessro@oregonstate.edu>
2024-05-15 12:27:12 -0700

    Simplify EventCard skeleton implementation.

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -10,7 +10,7 @@ import {
   Button,
   IconButton,
   Card, CardBody,
-  Skeleton,
+  SkeletonText,
   Popover, PopoverTrigger, PopoverContent, PopoverArrow, PopoverCloseButton, PopoverBody
 } from '@chakra-ui/react'
 import { FaAt, FaVideo, FaInfo } from 'react-icons/fa6'
@@ -28,62 +28,53 @@ export default function EventCard({
   extraInfo,
   skeleton
 }) {
-  /*
-   * Allow a skeleton card to be created without specifying values for other
-   * props.  Fill in dummy values for day, start, and location if they're not
-   * specified and we're creating a skeleton card.
-   */
-  day = (skeleton && !day) ? "Saturday" : day
-  start = (skeleton && !start) ? new Date() : start
-  location = (skeleton && !location) ? "KEC 1109" : location
-
   const startTime = dayjs(start, "HH:mm:ss").format("h:mm a")
   const endTime = end && dayjs(end, "HH:mm:ss").format("h:mm a")
 
   return (
     <Card p={2}>
       <CardBody>
-        <Skeleton isLoaded={!skeleton}>
-          <HStack align="baseline" mb={1}>
-            <Heading as="h3" size="md">{day}</Heading>
-            {extraInfo && (
-              <Popover>
-                <PopoverTrigger>
-                  <IconButton icon={<FaInfo />} arial-label="Extra info" isRound={true} size="xs" variant="outline" ml={1} />
-                </PopoverTrigger>
-                <PopoverContent>
-                  <PopoverArrow />
-                  <PopoverCloseButton />
-                  <PopoverBody>{extraInfo}</PopoverBody>
-                </PopoverContent>
-              </Popover>
-            )}
-          </HStack>
-        </Skeleton>
-        <Skeleton isLoaded={!skeleton}>
-          <Heading as="h4" size="sm" color="gray.600" mb={1}>{startTime}{endTime && <> &ndash; {endTime}</>}</Heading>
-        </Skeleton>
-        <Skeleton isLoaded={!skeleton}>
-          <HStack align="center">
-            {location && (
-              <>
-                <Icon as={FaAt} />
-                <Text fontSize="medium"> {location}</Text>
-              </>
-            )}
-            {videoconferenceLink && (
-              <Button
-                leftIcon={<FaVideo />}
-                size="xs"
-                variant="outline"
-                as="a"
-                href={videoconferenceLink}
-              >
-                Join
-              </Button>
-            )}
-          </HStack>
-        </Skeleton>
+        {skeleton ? (
+          <SkeletonText noOfLines={3} spacing={1} skeletonHeight={5} minW={24} />
+        ) : (
+          <>
+            <HStack align="baseline" mb={1}>
+              <Heading as="h3" size="md">{day}</Heading>
+              {extraInfo && (
+                <Popover>
+                  <PopoverTrigger>
+                    <IconButton icon={<FaInfo />} arial-label="Extra info" isRound={true} size="xs" variant="outline" ml={1} />
+                  </PopoverTrigger>
+                  <PopoverContent>
+                    <PopoverArrow />
+                    <PopoverCloseButton />
+                    <PopoverBody>{extraInfo}</PopoverBody>
+                  </PopoverContent>
+                </Popover>
+              )}
+            </HStack>
+            <Heading as="h4" size="sm" color="gray.600" mb={1}>{startTime}{endTime && <> &ndash; {endTime}</>}</Heading>
+            <HStack align="center">
+              {location && (
+                <>
+                  <Icon as={FaAt} />
+                  <Text fontSize="medium"> {location}</Text>
+                </>
+              )}
+              {videoconferenceLink && (
+                <Button
+                  leftIcon={<FaVideo />}
+                  size="xs"
+                  variant="outline"
+                  as="a"
+                  href={videoconferenceLink}
+                >
+                  Join
+                </Button>
+              )}
+            </HStack>
+          </>
+        )}
       </CardBody>
     </Card>
   )
