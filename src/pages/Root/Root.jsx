2f146be697b23d2420d122dffa5ffd2974dc522b
Rob Hess <hessro@oregonstate.edu>
2024-12-18 10:21:31 -0500

    Use Container to limit width of application content.

--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -3,14 +3,15 @@
  */
 
 import { Outlet } from 'react-router'
+import { Container } from '@chakra-ui/react'
 
 import Header from './components/Header'
 
 export default function Root() {
   return (
-    <>
+    <Container maxW="8xl">
       <Header />
       <Outlet />
-    </>
+    </Container>
   )
 }
