193391805cf30ce1c9c13dd6d8e4e7a188fe49aa
Rob Hess <hessro@oregonstate.edu>
2024-12-18 09:46:45 -0500

    Fix indentation in course page and course/term page.

--- src/pages/Course/Course.jsx
+++ src/pages/Course/Course.jsx
@@ -12,31 +12,31 @@ import { COURSE_PAGE_COURSE } from '@/lib/apollo/queries'
 import { getTermYearCode } from '@/lib/database/utils'
 
 export default function Course() {
-    const { course } = useParams()
-    const { data } = useQuery(COURSE_PAGE_COURSE, {
-      fetchPolicy: 'cache-only',
-      variables: { id: course }
-    })
-    const courseData = data?.courseCollection?.edges[0]?.node
-    const courseTerms = courseData?.courseTermCollection?.edges
-    const currentTerm = courseTerms[0]?.node
-    if (currentTerm) {
-      return <Navigate to={`${getTermYearCode(currentTerm.term, currentTerm.year)}`} />
-    } else {
-      return (
-        <Box px={6}>
-          <Heading as="h1" size="md" mt={6}>
-            {courseData?.number} &ndash; {courseData?.title}
-          </Heading>
-          <Text fontSize={["sm", "md"]} color="gray.600">
-            {courseData?.description}
-          </Text>
+  const { course } = useParams()
+  const { data } = useQuery(COURSE_PAGE_COURSE, {
+    fetchPolicy: 'cache-only',
+    variables: { id: course }
+  })
+  const courseData = data?.courseCollection?.edges[0]?.node
+  const courseTerms = courseData?.courseTermCollection?.edges
+  const currentTerm = courseTerms[0]?.node
+  if (currentTerm) {
+    return <Navigate to={`${getTermYearCode(currentTerm.term, currentTerm.year)}`} />
+  } else {
+    return (
+      <Box px={6}>
+        <Heading as="h1" size="md" mt={6}>
+          {courseData?.number} &ndash; {courseData?.title}
+        </Heading>
+        <Text fontSize={["sm", "md"]} color="gray.600">
+          {courseData?.description}
+        </Text>
 
-          <Alert status="warning" variant="left-accent" mt={8}>
-            <AlertIcon />
-            <AlertDescription>There are no recent offerings of this course to display.</AlertDescription>
-          </Alert>
-        </Box>
-      )
-    }
+        <Alert status="warning" variant="left-accent" mt={8}>
+          <AlertIcon />
+          <AlertDescription>There are no recent offerings of this course to display.</AlertDescription>
+        </Alert>
+      </Box>
+    )
+  }
 }
--- src/pages/CourseTerm/CourseTerm.jsx
+++ src/pages/CourseTerm/CourseTerm.jsx
@@ -13,29 +13,29 @@ import { terms } from '@/lib/database/strings'
 import { getCourseTermId } from '@/lib/database/utils'
 
 export default function CourseTerm() {
-    const { course, termYear } = useParams()
-    const currCourseTerm = getCourseTermId(course, termYear)
-    const { data } = useQuery(COURSE_TERM_PAGE_COURSE, {
-      fetchPolicy: 'cache-only',
-      variables: { courseId: course, courseTermId: currCourseTerm }
-    })
-    const courseData = data?.courseCollection?.edges[0]?.node
-    const courseTerms = courseData?.courseTermCollection?.edges
-    return (
-      <Box px={6}>
-        <Heading as="h1" size="md" mt={6}>
-          {courseData?.number} &ndash; {courseData?.title}
-        </Heading>
-        <Text fontSize={["sm", "md"]} color="gray.600">
-          {courseData?.description}
-        </Text>
-        <Select mt={4} defaultValue={currCourseTerm}>
-          {courseTerms?.map(courseTerm => (
-            <option key={courseTerm.node.id} value={courseTerm.node.id}>
-              {terms[courseTerm.node.term]} {courseTerm.node.year}
-            </option>
-          ))}
-        </Select>
-      </Box>
-    )
-  }
+  const { course, termYear } = useParams()
+  const currCourseTerm = getCourseTermId(course, termYear)
+  const { data } = useQuery(COURSE_TERM_PAGE_COURSE, {
+    fetchPolicy: 'cache-only',
+    variables: { courseId: course, courseTermId: currCourseTerm }
+  })
+  const courseData = data?.courseCollection?.edges[0]?.node
+  const courseTerms = courseData?.courseTermCollection?.edges
+  return (
+    <Box px={6}>
+      <Heading as="h1" size="md" mt={6}>
+        {courseData?.number} &ndash; {courseData?.title}
+      </Heading>
+      <Text fontSize={["sm", "md"]} color="gray.600">
+        {courseData?.description}
+      </Text>
+      <Select mt={4} defaultValue={currCourseTerm}>
+        {courseTerms?.map(courseTerm => (
+          <option key={courseTerm.node.id} value={courseTerm.node.id}>
+            {terms[courseTerm.node.term]} {courseTerm.node.year}
+          </option>
+        ))}
+      </Select>
+    </Box>
+  )
+}
