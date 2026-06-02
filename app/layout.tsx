417bcb3e7fa3176649fd9467c462fc8922fe152e
Rob Hess <hessro@oregonstate.edu>
2026-06-02 14:23:04 -0700

    Update code style.  Use ESLint stylistic plugin to enforce code style.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from "next"
+import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "Rob Hess - Oregon State University"
-};
+    title: "Rob Hess - Oregon State University"
+}
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="en" className="h-full antialiased">
-      <body className="min-h-full">{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en" className="h-full antialiased">
+            <body className="min-h-full">{children}</body>
+        </html>
+    )
 }
