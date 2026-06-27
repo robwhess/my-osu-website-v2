5f2f786c1b80f1b079b589e45e0800362590fe5e
Rob Hess <hessro@oregonstate.edu>
2026-06-27 17:15:22 -0500

    Finalize courses page content.

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -1,3 +1,17 @@
+import { FaRegHandPointLeft, FaRegHandPointUp } from "react-icons/fa6"
+
 export default async function CoursesPage() {
-    return <h1 className="mx-auto my-20">Courses</h1>
+    return (
+        <>
+            <div className="max-md:hidden mt-24 flex justify-center items-center gap-2 text-2xl text-gray-400">
+                <FaRegHandPointLeft />
+                <p className="text-xl">Choose a course using the menu to the left</p>
+            </div>
+            <div className="md:hidden mt-24 flex flex-col justify-center items-center gap-2 text-2xl text-gray-400">
+                <FaRegHandPointUp />
+                <p className="text-lg text-center">Choose a course using the menu above</p>
+            </div>
+        </>
+
+    )
 }
