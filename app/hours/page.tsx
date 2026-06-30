3dcb4cd736b070feeb63b1760455595118b3c1bc
Rob Hess <hessro@oregonstate.edu>
2026-06-30 10:31:35 -0500

    Add loading state for office hours.

--- app/hours/page.tsx
+++ app/hours/page.tsx
@@ -16,12 +16,19 @@ export default function HoursPage() {
             .match({ person_id: ROBS_PERSON_ID, type: "office" })
     )
 
-    console.log("== isLoading:", isLoading)
-
     return (
         <main className="flex flex-col items-center mx-2 my-12">
             <h1 className="mb-6 md:mb-4 text-3xl md:text-4xl font-medium">Rob&apos;s Office Hours</h1>
             <div className="flex justify-center flex-wrap flex-col w-full max-w-xl gap-2">
+                {isLoading && (
+                    <div className="card bg-base-100 shadow-sm">
+                        <div className="card-body gap-2">
+                            <div className="skeleton h-6 w-3/4"></div>
+                            <div className="skeleton h-4 w-1/2"></div>
+                            <div className="skeleton h-4 w-2/3"></div>
+                        </div>
+                    </div>
+                )}
                 {data && data.map(hours => <EventCard key={hours.id} {...hours} />)}
             </div>
         </main>
